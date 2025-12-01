5e2c5a2351a4bfdf154ddd8df4a8e590825ec0f7
Tassio Neves <tassions@hotmail.com>
2025-12-01 12:31:43 -0300

    feat: update DataTable to parse products data and add safa-actions client

--- app/(dashboard)/products/page.tsx
+++ app/(dashboard)/products/page.tsx
@@ -23,7 +23,10 @@ const ProductsPage = async () => {
         </Button>
       </div>
 
-      <DataTable columns={productTableColumns} data={products} />
+      <DataTable
+        columns={productTableColumns}
+        data={JSON.parse(JSON.stringify(products))}
+      />
     </div>
   );
 };
