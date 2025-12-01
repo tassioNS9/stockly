95a97b6f80cedfef1aad5e65603aa9823491a408
Tassio Neves <tassions@hotmail.com>
2025-12-01 12:31:01 -0300

    feat: add product

---
+++ app/_actions/product/upsert-product/index.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { revalidatePath } from "next/cache";
+import { upsertProductSchema } from "./schema";
+import { actionClient } from "@/app/_lib/safa-actions";
+
+export const upsertProduct = actionClient
+  .schema(upsertProductSchema)
+  .action(async ({ parsedInput: { id, ...data } }) => {
+    upsertProductSchema.parse(data);
+    await db.product.upsert({
+      where: { id: id ?? "" },
+      update: data,
+      create: data,
+    });
+    revalidatePath("/products", "page");
+    revalidatePath("/");
+  });
