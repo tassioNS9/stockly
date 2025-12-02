f501e5bd0db9c5cbe518bcd18d1122636a8cc310
Tassio Neves <tassions@hotmail.com>
2025-12-02 16:19:58 -0300

    feat: product stock is validated before  adding product to sale

--- app/(dashboard)/sales/_components/upsert-sheet-content.tsx
+++ app/(dashboard)/sales/_components/upsert-sheet-content.tsx
@@ -83,6 +83,15 @@ const UpsertSheetContent = ({
         (product) => product.id === selectedProduct.id,
       );
       if (existingProduct) {
+        const productIsOutOfStock =
+          existingProduct.quantity + data.quantity > selectedProduct.stock;
+        if (productIsOutOfStock) {
+          form.setError("quantity", {
+            message: "Quantidade indisponível em estoque!",
+          });
+          return currentProducts;
+        }
+        form.reset();
         return currentProducts.map((product) => {
           if (product.id === selectedProduct.id) {
             return {
@@ -93,6 +102,15 @@ const UpsertSheetContent = ({
           return product;
         });
       }
+
+      const productIsOutOfStock = data.quantity > selectedProduct.stock;
+      if (productIsOutOfStock) {
+        form.setError("quantity", {
+          message: "Quantidade indisponível em estoque!",
+        });
+        return currentProducts;
+      }
+      form.reset();
       return [
         ...currentProducts,
         {
@@ -102,7 +120,6 @@ const UpsertSheetContent = ({
         },
       ];
     });
-    form.reset();
   };
 
   const productsTotal = useMemo(() => {
