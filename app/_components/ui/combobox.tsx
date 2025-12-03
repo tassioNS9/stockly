507c341560d5d38a160842fa4fcae02605e56a63
Tassio Neves <tassions@hotmail.com>
2025-12-03 15:49:16 -0300

    feat: make value prop optional in ComboboxProps

--- app/_components/ui/combobox.tsx
+++ app/_components/ui/combobox.tsx
@@ -22,7 +22,7 @@ export interface ComboboxOption {
 
 interface ComboboxProps {
   options: ComboboxOption[];
-  value: string;
+  value?: string;
   onChange: (value: string) => void;
   placeholder?: string;
 }
