4d712475482827fd8c3e92b25492757dfed181dd
Tassio Neves <tassions@hotmail.com>
2025-12-04 14:52:51 -0300

    feat: ensure initial cards dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,68 @@
+import {
+  DollarSign,
+  CircleDollarSign,
+  PackageIcon,
+  ShoppingBasketIcon,
+} from "lucide-react";
+import {
+  SummaryCard,
+  SummaryCardIcon,
+  SummaryCardTitle,
+  SummaryCardValue,
+} from "./_components/summary-card";
+
 export default function Home() {
-  return <div>HomePage</div>;
+  return (
+    <div className="mx-8 my-8 w-full space-y-8 rounded-lg p-8">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Dashboard
+          </span>
+          <h2 className="text-xl font-semibold">Dashboard</h2>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-6">
+        <SummaryCard>
+          <SummaryCardIcon>
+            <DollarSign />
+          </SummaryCardIcon>
+          <SummaryCardTitle>Receita Total</SummaryCardTitle>
+          <SummaryCardValue>1040</SummaryCardValue>
+        </SummaryCard>
+
+        <SummaryCard>
+          <SummaryCardIcon>
+            <DollarSign />
+          </SummaryCardIcon>
+          <SummaryCardTitle>Receita Hoje</SummaryCardTitle>
+          <SummaryCardValue>20.000</SummaryCardValue>
+        </SummaryCard>
+      </div>
+      <div className="grid grid-cols-3 gap-6">
+        <SummaryCard>
+          <SummaryCardIcon>
+            <CircleDollarSign />
+          </SummaryCardIcon>
+          <SummaryCardTitle>Vendas Totais</SummaryCardTitle>
+          <SummaryCardValue>20.000</SummaryCardValue>
+        </SummaryCard>
+        <SummaryCard>
+          <SummaryCardIcon>
+            <PackageIcon />
+          </SummaryCardIcon>
+          <SummaryCardTitle>Total em Estoque</SummaryCardTitle>
+          <SummaryCardValue>450</SummaryCardValue>
+        </SummaryCard>
+        <SummaryCard>
+          <SummaryCardIcon>
+            <ShoppingBasketIcon />
+          </SummaryCardIcon>
+          <SummaryCardTitle>Produtos</SummaryCardTitle>
+          <SummaryCardValue>250</SummaryCardValue>
+        </SummaryCard>
+      </div>
+    </div>
+  );
 }
