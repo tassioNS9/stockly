9f01efd8d6b78b080231a03c2830b88321310f13
TassioNS9 <tassions@hotmail.com>
2025-12-08 11:55:35 -0300

    feat: add initial revenue chart

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,13 @@ import {
 } from "./_components/summary-card";
 import { getDashboard } from "./_data_access/dashboard/get-dashboard";
 import { formatCurrency } from "./_helpers/currency";
+import RevenueChart from "./(dashboard)/_components/revenue-chart";
+import { getLast14DaysRevenue } from "./_data_access/dashboard/get-last-14-days-revenue";
 const Home = async () => {
   const { totalRevenue, todayRevenue, totalSales, totalStock, totalProducts } =
     await getDashboard();
 
+  const totalLast14DaysRevenue = await getLast14DaysRevenue();
   return (
     <div className="mx-8 my-8 w-full space-y-8 rounded-lg p-8">
       <div className="flex w-full items-center justify-between">
@@ -66,6 +69,14 @@ const Home = async () => {
           <SummaryCardTitle>Produtos</SummaryCardTitle>
           <SummaryCardValue>{totalProducts}</SummaryCardValue>
         </SummaryCard>
+      </div>
+
+      <div className="grid min-h-0 grid-cols-[minmax(0,2.5fr),minmax(0,1fr)] gap-6">
+        <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white p-6">
+          <p className="text-lg font-semibold text-slate-900">Receita</p>
+          <p className="text-sm text-slate-400">Últimos 14 dias</p>
+          <RevenueChart data={totalLast14DaysRevenue} />
+        </div>
       </div>
     </div>
   );
